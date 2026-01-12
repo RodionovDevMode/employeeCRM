45d1a22ab9bbb25d207068c4c27d1d0cdb77e5fd
Andrei Rodionov <rodionovdevmode@MacBook-Air-Andrei.local>
2026-01-12 14:10:49 +0300

    Set up project structure and initial component templates

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 
-import App from './App.tsx'
+import App from './components/app/App'
 import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')!).render(
